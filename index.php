4ec7ba93c3a1dc3182dd29302c362debc9764ed6
maheshfinpros <166098099+maheshfinpros@users.noreply.github.com>
2024-04-05 16:39:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>PHP Demo App</title>
-</head>
-<body>
-
 <?php
-// Displaying text using PHP
-echo "<h1>Welcome to PHP Demo App!</h1>";
-echo "<p>This is a simple PHP application demonstrating text display.</p>";
-echo "<p>Feel free to modify and expand upon this script to create your own PHP applications!</p>";
+// PHP code goes here
+echo "Hello, World!";
 ?>
-
-</body>
-</html>
-
