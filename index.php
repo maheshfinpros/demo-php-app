aa5db5d2b034b1247b86883ca72d99ef1a83b994
Ubuntu <ubuntu@ip-172-31-36-177.ap-south-1.compute.internal>
2024-04-05 07:39:57 +0000

    first

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Demo App</title>
+</head>
+<body>
+
+<?php
+// Displaying text using PHP
+echo "<h1>Welcome to PHP Demo App!</h1>";
+echo "<p>This is a simple PHP application demonstrating text display.</p>";
+echo "<p>Feel free to modify and expand upon this script to create your own PHP applications!</p>";
+?>
+
+</body>
+</html>
+
